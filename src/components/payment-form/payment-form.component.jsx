d67da321a71ea1ed45e5afee88a1422cd538f8c6
mjkavanagh <mjkavanagh7@yahoo.com>
2023-05-11 09:18:16 -0400

    add Stripe API for card checkout

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -22,7 +22,7 @@ const PaymentForm = () => {
     }
     setIsProcessingPayment(true);
 
-    const response = await fetch("/,netlify/functions/create-payment-intent", {
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
